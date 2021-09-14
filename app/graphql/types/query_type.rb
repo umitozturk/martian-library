e9a51ea51bc8e5b1274a912a4e6dbd01bf5d7c18
Ümit Öztürk <ozturkumit34@gmail.com>
2021-09-14 22:27:17 +0300

    initial commit

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,13 @@
+# app/graphql/types/query_type.rb
+module Types
+  class QueryType < Types::BaseObject
+    field :items,
+          [Types::ItemType],
+          null: false,
+          description: "Returns a list of items in the martian library"
+
+    def items
+      Item.preload(:user)
+    end
+  end
+end
